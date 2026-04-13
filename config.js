5b1551a0c2ca69269081da8708e0fddc99b69657
wesleymarcelino3000-cmd <wesleymarcelino3000@gmail.com>
2026-04-13 16:02:21 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export const SUPABASE_URL = "https://rzfrazzxsntckxlwoifq.supabase.co";
 export const SUPABASE_KEY = "sb_publishable_DmDCZgfasGNf56oXd3gxyg_qOFzZ2Vx";
 export const LOCAL_EMPRESA = { lat: -20.473881, lng: -45.793709 };
-export const LIMITE_METROS = 50;
+export const LIMITE_METROS = 100;
+export const TIMEZONE = "America/Sao_Paulo";
